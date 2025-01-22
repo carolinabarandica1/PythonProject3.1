249883eb60d990b9e4a610a79643c01bbba26d4c
carolinabarandica <carolinabarandica1@gmail.com>
2025-01-22 17:57:53 +0100

    new commit of the day

--- .venv/if_example.py
+++ .venv/if_example.py
@@ -1,5 +1,11 @@
-a = 30
+a = 30 #single equal sign means 'takes the value of'
 b = 20
 if a > b :
     print(a,"is greater than b",b)
+
+elif a == b:
+    print(a,"is equal to", b)
+
+else:
+    print(a,"is less than", b)
 
