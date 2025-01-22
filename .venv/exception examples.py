dc8284b61c540704e44c3ae870f1b0e7a6c65ae1
carolinabarandica <carolinabarandica1@gmail.com>
2025-01-22 16:56:56 +0100

    new commit of the day

--- .venv/exception examples.py
+++ .venv/exception examples.py
@@ -1,4 +1,9 @@
 name = input("What is your name")
 print("hello", name)
-age = input("how old are you?")
-print("you were probably born in", 2024 - int(age))
+age = input("how old are you? ")
+try:
+    age = int(age)  # i'm trying to convert it to a number
+    print("you were probably born in", 2024 - int(age))
+
+except:
+    print("you are trying to trick me")
