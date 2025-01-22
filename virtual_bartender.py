266fefb6273806e44eb2c8a3f4dbcea0169360e0
carolinabarandica <carolinabarandica1@gmail.com>
2025-01-22 18:26:34 +0100

    new commit of the day

--- virtual_bartender.py
+++ virtual_bartender.py
@@ -1,3 +1,28 @@
 from random import choices
-drinks = ("gin" ,"vodka", "tequila", "sake")
-print(choices(drinks))
+from random import choices
+
+drinks = ("gin", "vodka", "tequila", "sake")
+name = input("I'm the virtual bartender. What is your name? ")
+try:
+    age = input("What is your age? ")
+    age = int(age)
+
+    country = input("What is your country? ")
+
+    legal = False
+    if age >= 21:
+        legal = True
+    elif age >= 18 and (country != "USA" and country != "UAE"):
+        legal = True
+    elif age >= 16 and country == "Luxembourg":
+        legal = True
+
+except ValueError:
+    print("Don't play games with me!")
+
+else:
+    if legal:
+        print("dear", name, "its a pleasure to serve you", choices(drinks))
+    else:
+        print("dear", name, "unfortunately i cant serve you")
+
